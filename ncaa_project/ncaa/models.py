9575d3834820d2a25f2410e1a6bd66e044e7aaa1
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2022-08-31 14:18:40 -0500

    full admin crud

--- ncaa_project/ncaa/models.py
+++ ncaa_project/ncaa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+# tunr/models.py
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    divison = models.CharField(max_length=100)
+    wins = models.PositiveSmallIntegerField()
+    losses = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    team = models.ForeignKey(
+        Team, on_delete=models.CASCADE, null=True, related_name='player')
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    injured_reserved_list = models.BooleanField()
+
+    def __str__(self):
+        return self.name
